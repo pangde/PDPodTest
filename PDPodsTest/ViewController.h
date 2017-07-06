902fdc83f01c9178c357be212874d17bb333a702
pangde <pangde@iMac.local>
2017-07-06 16:19:20 +0800

    podspec

---
+++ PDPodsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDBasic
+//
+//  Created by pangde on 17/3/1.
+//  Copyright © 2017年 pangde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PDPodsTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PDBasic
+//
+//  Created by pangde on 17/3/1.
+//  Copyright © 2017年 pangde. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
